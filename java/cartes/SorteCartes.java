b39491ae0f2b645058cd8ea83cb3dfcc7583da1f
wnfhfh <houdeantoine7@gmail.com>
2023-03-08 09:23:18 -0500

    Fichiers Prof

---
+++ java/cartes/SorteCartes.java
@@ -0,0 +1,11 @@
+package cartes;
+
+import java.awt.Color;
+
+public enum SorteCartes
+{
+    COEUR('♥', Color.RED),
+    CARREAU('\u2666', Color.RED),
+    PIQUE('♠', Color.BLACK),
+    TREFLE('♣', Color.BLACK);
+}
